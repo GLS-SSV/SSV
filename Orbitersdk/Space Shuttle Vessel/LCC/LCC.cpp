7c4a860a7627c498bb1e205a5325d24a895ca5a7
GLS-SSV <GLS-SSV@users.noreply.github.com>
2022-08-20 23:58:27 +0100

    added countdown time to countdown hold LCC log output

--- Orbitersdk/Space Shuttle Vessel/LCC/LCC.cpp
+++ Orbitersdk/Space Shuttle Vessel/LCC/LCC.cpp
@@ -16,6 +16,7 @@ Date         Developer
 2022/01/09   GLS
 2022/08/05   GLS
 2022/08/10   GLS
+2022/08/20   GLS
 ********************************************/
 #include "LCC.h"
 #include "LCC_MFD.h"
@@ -268,8 +269,8 @@ void LCC::clbkPreStep(double simt, double simdt, double mjd)
 		{
 			requestautohold = true;
 
-			if (pPad->GetRSCountdownHoldFlag()) oapiWriteLog( "(SSV_LCC) [INFO] RS Countdown Hold Flag is on" );
-			else oapiWriteLog( "(SSV_LCC) [INFO] Launch Sequence Abort Flag is on" );
+			if (pPad->GetRSCountdownHoldFlag()) oapiWriteLogV( "(SSV_LCC) [INFO] RS Countdown Hold Flag is on @ T%+.3f", CountdownTime );
+			else oapiWriteLogV( "(SSV_LCC) [INFO] Launch Sequence Abort Flag is on @ T%+.3f", CountdownTime );
 
 			unsigned int holdflags = pPad->GetHoldFlags();
 			if (holdflags & 0x00000001) oapiWriteLog( "(SSV_LCC) [INFO] Engine Shutdown Verification Hold" );
@@ -424,7 +425,7 @@ void LCC::Checks( void )
 		{
 			if (!requestautohold)
 			{
-				oapiWriteLog( "(SSV_LCC) [INFO] HYD-02 violation" );
+				oapiWriteLogV( "(SSV_LCC) [INFO] HYD-02 violation @ T%+.3f", CountdownTime );
 				requestautohold = true;
 			}
 		}
@@ -440,7 +441,7 @@ void LCC::Checks( void )
 		{
 			if (!requestautohold)
 			{
-				oapiWriteLog( "(SSV_LCC) [INFO] MPS-01 violation" );
+				oapiWriteLogV( "(SSV_LCC) [INFO] MPS-01 violation @ T%+.3f", CountdownTime );
 				requestautohold = true;
 			}
 		}
@@ -455,7 +456,7 @@ void LCC::Checks( void )
 		{
 			if (!requestautohold)
 			{
-				oapiWriteLog( "(SSV_LCC) [INFO] MPS-03 violation" );
+				oapiWriteLogV( "(SSV_LCC) [INFO] MPS-03 violation @ T%+.3f", CountdownTime );
 				requestautohold = true;
 			}
 		}
