9033a48fff9d486bbab48d919cda444cf8568c61
GLS-SSV <GLS-SSV@users.noreply.github.com>
2022-09-20 00:36:20 +0100

    >> corrected CTVM mesh
    >> separated CTVM cables

--- Orbitersdk/Space Shuttle Vessel/OV/meshres_vc_a3.h
+++ Orbitersdk/Space Shuttle Vessel/OV/meshres_vc_a3.h
@@ -5,23 +5,25 @@
 
 #pragma once
 
-inline constexpr unsigned int NUMGRP_A3_VC = 18;
+inline constexpr unsigned int NUMGRP_A3_VC = 20;
 
 inline constexpr unsigned int GRP_CCTVMONITORS_A3_VC	=	0;	//CCTVmonitors
-inline constexpr unsigned int GRP_MON1_A3_VC	=	1;	//MON1
-inline constexpr unsigned int GRP_MON1_S1_A3_VC	=	2;	//MON1_S1
-inline constexpr unsigned int GRP_MON1_S2_A3_VC	=	3;	//MON1_S2
-inline constexpr unsigned int GRP_MON1_R3_A3_VC	=	4;	//MON1_R3
-inline constexpr unsigned int GRP_MON1_R4_A3_VC	=	5;	//MON1_R4
-inline constexpr unsigned int GRP_MON1_R1_A3_VC	=	6;	//MON1_R1
-inline constexpr unsigned int GRP_MON1_R2_A3_VC	=	7;	//MON1_R2
-inline constexpr unsigned int GRP_MON1_S3_A3_VC	=	8;	//MON1_S3
-inline constexpr unsigned int GRP_MON2_A3_VC	=	9;	//MON2
-inline constexpr unsigned int GRP_MON2_S1_A3_VC	=	10;	//MON2_S1
-inline constexpr unsigned int GRP_MON2_S2_A3_VC	=	11;	//MON2_S2
-inline constexpr unsigned int GRP_MON2_R3_A3_VC	=	12;	//MON2_R3
-inline constexpr unsigned int GRP_MON2_R4_A3_VC	=	13;	//MON2_R4
-inline constexpr unsigned int GRP_MON2_R1_A3_VC	=	14;	//MON2_R1
-inline constexpr unsigned int GRP_MON2_R2_A3_VC	=	15;	//MON2_R2
-inline constexpr unsigned int GRP_MON2_S3_A3_VC	=	16;	//MON2_S3
-inline constexpr unsigned int GRP_OVERLAY_A3_VC	=	17;	//overlay
+inline constexpr unsigned int GRP_MON1_D_CABLE_A3_VC	=	1;	//MON1_D_cable
+inline constexpr unsigned int GRP_MON2_D_CABLE_A3_VC	=	2;	//MON2_D_cable
+inline constexpr unsigned int GRP_MON1_A3_VC	=	3;	//MON1
+inline constexpr unsigned int GRP_MON1_S1_A3_VC	=	4;	//MON1_S1
+inline constexpr unsigned int GRP_MON1_S2_A3_VC	=	5;	//MON1_S2
+inline constexpr unsigned int GRP_MON1_R3_A3_VC	=	6;	//MON1_R3
+inline constexpr unsigned int GRP_MON1_R4_A3_VC	=	7;	//MON1_R4
+inline constexpr unsigned int GRP_MON1_R1_A3_VC	=	8;	//MON1_R1
+inline constexpr unsigned int GRP_MON1_R2_A3_VC	=	9;	//MON1_R2
+inline constexpr unsigned int GRP_MON1_S3_A3_VC	=	10;	//MON1_S3
+inline constexpr unsigned int GRP_MON2_A3_VC	=	11;	//MON2
+inline constexpr unsigned int GRP_MON2_S1_A3_VC	=	12;	//MON2_S1
+inline constexpr unsigned int GRP_MON2_S2_A3_VC	=	13;	//MON2_S2
+inline constexpr unsigned int GRP_MON2_R3_A3_VC	=	14;	//MON2_R3
+inline constexpr unsigned int GRP_MON2_R4_A3_VC	=	15;	//MON2_R4
+inline constexpr unsigned int GRP_MON2_R1_A3_VC	=	16;	//MON2_R1
+inline constexpr unsigned int GRP_MON2_R2_A3_VC	=	17;	//MON2_R2
+inline constexpr unsigned int GRP_MON2_S3_A3_VC	=	18;	//MON2_S3
+inline constexpr unsigned int GRP_OVERLAY_A3_VC	=	19;	//overlay
